aa7b5933d5041d00a616af73e19902ed87d04b4e
LeDfee <yermakovdo@gmail.com>
2023-03-30 21:08:42 +0200

    Update Phone.java

--- src/main/java/project/Phone/Maven/Phone.java
+++ src/main/java/project/Phone/Maven/Phone.java
@@ -1,2 +1,70 @@
-package project.Phone.Maven;public class Phone {
+package project.Phone.Maven;
+
+import java.util.Scanner;
+
+public class Phone {
+    private String number, model;
+    private double weight;
+
+    public Phone(String number, String model, double weight){
+        this.number = number;
+        this.model = model;
+        this.weight = weight;
+    }
+
+    public Phone(String number, String model){
+        this(number, model, 0);
+    }
+
+    public Phone(){
+        this("", "", 0);
+    }
+
+    public String getNumber(){
+        return number;
+    }
+
+    public void receiveCall(String name){
+        System.out.println(name + " is calling");
+    }
+
+    public void receiveCall(String name, String callerNumber){
+        System.out.println(name + " is calling " + callerNumber);
+    }
+
+    public void sendMessage(String... numbers){
+        System.out.println("Send a message to: ");
+        for(String number : numbers){
+            System.out.println(number + " ");
+        }
+        System.out.println();
+    }
+
+    public String getModel(){
+        return model;
+    }
+
+    public void setModel(String model){
+        this.model = model;
+    }
+
+    public double getWeight(){
+        return weight;
+    }
+
+    public void setWeight(double weight){
+        this.weight = weight;
+    }
+
+    public class Main{
+        public void main(String[] args){
+            Scanner scanner = new Scanner(System.in);
+            String number = scanner.nextLine();
+            String model = scanner.nextLine();
+            double weight = scanner.nextDouble();
+            Phone phone = new Phone(number, model, weight);
+            String callerName = scanner.next();
+            phone.receiveCall(callerName);
+        }
+    }
 }
