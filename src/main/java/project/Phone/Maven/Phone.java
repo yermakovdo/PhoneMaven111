919dcec979867a1fae3ae8ee825e5962468f7c60
LeDfee <yermakovdo@gmail.com>
2023-04-01 20:25:41 +0200

    Update Phone.java
    second commit

--- src/main/java/project/Phone/Maven/Phone.java
+++ src/main/java/project/Phone/Maven/Phone.java
@@ -55,16 +55,4 @@ public class Phone {
     public void setWeight(double weight){
         this.weight = weight;
     }
-
-    public class Main{
-        public void main(String[] args){
-            Scanner scanner = new Scanner(System.in);
-            String number = scanner.nextLine();
-            String model = scanner.nextLine();
-            double weight = scanner.nextDouble();
-            Phone phone = new Phone(number, model, weight);
-            String callerName = scanner.next();
-            phone.receiveCall(callerName);
-        }
-    }
 }
